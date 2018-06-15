98ec39164a0725ff8f5570a0eca7156d624adfae
Mads Østergaard <m@madsostergaard.dk>
2018-06-15 14:27:17 +0200

    Arbejde med kalenderen

---
+++ MediCue/CustomCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCell.swift
+//  MediCue
+//
+//  Created by Mads Østergaard on 15/06/2018.
+//  Copyright © 2018 Mads Østergaard. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTAppleCell {
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var selectedView: UIView!
+}
